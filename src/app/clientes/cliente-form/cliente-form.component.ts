edced3852f256552b6bbd6137dd3c4b45bc079fb
Josivaldo Batista <josivaldo-fb@outlook.com>
2020-08-04 18:51:57 -0300

    Criando formulário de cadastro de Cliente.

--- src/app/clientes/cliente-form/cliente-form.component.ts
+++ src/app/clientes/cliente-form/cliente-form.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cliente } from '../cliente';
+ 
 @Component({
   selector: 'app-cliente-form',
   templateUrl: './cliente-form.component.html',
   styleUrls: ['./cliente-form.component.css']
 })
 export class ClienteFormComponent implements OnInit {
+
+  cliente: Cliente;
 
   constructor() { }
 
